dc9cb4a486623e4dd9569c0f18f2706e9daf2d71
Daniel Batista <dani_batista20@hotmail.com>
2024-05-31 00:15:47 +0100

    add: placeholder-data, spinner.svg & upd: globals

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,194 @@
+export const data = {
+  expansions: [
+    {
+      // punk expansion
+      name: 'punk',
+      cards: [
+        // array of card objects
+        {
+          id: '7fd7b269-5c40-49be-8cd7-6e40cdc4166d',
+          name: 'Rogue',
+          image: {
+            original: '/assets/cards/_7fd7b269-5c40-49be-8cd7-6e40cdc4166d.jpg',
+          },
+          kind: 'punk',
+          attack: 8,
+          defense: 4,
+          summon: 2,
+        },
+        {
+          id: '_9d04a281-0192-43ca-904b-f88c22bbff03',
+          name: 'Barbie',
+          image: {
+            original: '/assets/cards/_9d04a281-0192-43ca-904b-f88c22bbff03.jpg',
+          },
+          kind: 'punk',
+          attack: 6,
+          defense: 3,
+          summon: 1,
+        },
+        {
+          id: '_13b60ccc-795a-46fd-b480-6cd517121a2d',
+          name: 'Titan',
+          image: {
+            original: '/assets/cards/_13b60ccc-795a-46fd-b480-6cd517121a2d.jpg',
+          },
+          kind: 'punk',
+          attack: 4,
+          defense: 16,
+          summon: 4,
+        },
+        {
+          id: '_57c37e52-699f-40b0-b65f-d975dad666de',
+          name: 'Silver',
+          image: {
+            original: '/assets/cards/_57c37e52-699f-40b0-b65f-d975dad666de.jpg',
+          },
+          kind: 'punk',
+          attack: 6,
+          defense: 12,
+          summon: 4,
+        },
+        {
+          id: '_57d030e0-9a4c-416a-ab37-8140698a85aa',
+          name: 'Punked',
+          image: {
+            original: '/assets/cards/_57d030e0-9a4c-416a-ab37-8140698a85aa.jpg',
+          },
+          kind: 'punk',
+          attack: 8,
+          defense: 7,
+          summon: 2,
+        },
+        {
+          id: '_97ad2b82-259f-45ee-bec3-aec4acdaee2a',
+          name: 'Sakura',
+          image: {
+            original: '/assets/cards/_97ad2b82-259f-45ee-bec3-aec4acdaee2a.jpg',
+          },
+          kind: 'punk',
+          attack: 6,
+          defense: 6,
+          summon: 2,
+        },
+        {
+          id: '_a72f846b-b0c2-4834-8a12-029c9d1854c8',
+          name: 'Cyborg',
+          image: {
+            original: '/assets/cards/_a72f846b-b0c2-4834-8a12-029c9d1854c8.jpg',
+          },
+          kind: 'punk',
+          attack: 8,
+          defense: 6,
+          summon: 3,
+        },
+        {
+          id: '_a9052332-1a7b-4fac-83ac-dba4b7330de8',
+          name: 'Ken',
+          image: {
+            original: '/assets/cards/_a9052332-1a7b-4fac-83ac-dba4b7330de8.jpg',
+          },
+          kind: 'punk',
+          attack: 7,
+          defense: 4,
+          summon: 1,
+        },
+        {
+          id: '_ab5c3da4-3e2f-4d52-9c16-910b59fda219',
+          name: 'Panther',
+          image: {
+            original: '/assets/cards/_ab5c3da4-3e2f-4d52-9c16-910b59fda219.jpg',
+          },
+          kind: 'punk',
+          attack: 8,
+          defense: 4,
+          summon: 1,
+        },
+        {
+          id: '_aba8f409-bd0e-4986-946c-7d290915830a',
+          name: 'Cyan',
+          image: {
+            original: '/assets/cards/_aba8f409-bd0e-4986-946c-7d290915830a.jpg',
+          },
+          kind: 'punk',
+          attack: 5,
+          defense: 9,
+          summon: 1,
+        },
+        {
+          id: '_b9792434-9b07-47f3-b565-59c6fd2d16f6',
+          name: 'Optimus',
+          image: {
+            original: '/assets/cards/_b9792434-9b07-47f3-b565-59c6fd2d16f6.jpg',
+          },
+          kind: 'punk',
+          attack: 3,
+          defense: 18,
+          summon: 1,
+        },
+      ],
+      heroes: [
+        // array of hero objects
+        {
+          id: '_0eb47786-204d-49b1-ae9f-29221194dae0',
+          name: 'Ryo',
+          image: {
+            original: '/assets/cards/_0eb47786-204d-49b1-ae9f-29221194dae0',
+          },
+          kind: 'punk',
+          defense: 10,
+          power: 'All punks get + 1 Attack',
+        },
+        {
+          id: '_c1757a36-a8a4-49ec-9831-032f556b4602',
+          name: 'Cage',
+          image: {
+            original: '_c1757a36-a8a4-49ec-9831-032f556b4602',
+          },
+          kind: 'punk',
+          defense: 9,
+          power: 'All punks get + 1 Defense',
+        },
+        {
+          id: '_e9517b5b-3d57-454c-bdfa-cfd492bd0216',
+          name: 'Neo',
+          image: {
+            original: '_e9517b5b-3d57-454c-bdfa-cfd492bd0216',
+          },
+          kind: 'punk',
+          defense: 12,
+          power: 'All punks get + 1 Defense',
+        },
+        {
+          id: '_e9517b5b-3d57-454c-bdfa-cfd492bd0216',
+          name: 'John',
+          image: {
+            original: '_e9517b5b-3d57-454c-bdfa-cfd492bd0216',
+          },
+          kind: 'punk',
+          defense: 8,
+          power: '...',
+        },
+      ],
+      citys: [
+        {
+          id: '714b3c1b-db55-4fb6-aa59-13529ee0f83a',
+          name: 'New York',
+          image: {
+            original: '/assets/cards/714b3c1b-db55-4fb6-aa59-13529ee0f83a.jpg',
+          },
+          kind: 'punk',
+        },
+        {
+          id: 'f859f118-4971-42db-8d34-8f05631856c9',
+          name: 'Tokyo',
+          image: {
+            original: '/assets/cards/f859f118-4971-42db-8d34-8f05631856c9.jpg',
+          },
+          kind: 'punk',
+        },
+      ],
+    },
+    // more expansions can be added here
+  ],
+}
