cbd84476b0a0c45640ba47f3ddf0437857c921a3
Daniel Batista <dani_batista20@hotmail.com>
2024-05-31 02:09:31 +0100

    start: cards - page

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -1,10 +1,11 @@
-// page
+import { CardWrapper } from '@/components/cards'
 import { data } from '@/app/lib/placeholder-data'
 
-export default function Page() {
+export default function Cards() {
+  const cards = data.expansions[0].cards
   return (
     <main className="flex w-full h-auto flex-row items-center justify-between p-5 sm:p-16 bg-gray-400/5">
-      Home
+      <CardWrapper cards={cards} />
     </main>
   )
 }
